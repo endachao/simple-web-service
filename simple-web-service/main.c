61b9f7a2d8e1f8a97330cf2f94ff45e4c9cea84d
袁超 <yuanchao@syswin.com>
2016-09-28 15:28:09 +0800

    init.

---
+++ simple-web-service/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple-web-service
+//
+//  Created by yuanchao on 2016/9/28.
+//  Copyright © 2016年 yuanchao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
